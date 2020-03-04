3ce0e30263a03bc9a7e1e3e3cf93d76d3a73dc7b
Brian Nicholls <nichol88@gmail.com>
2020-03-04 18:44:40 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(attributes)
+    unless attributes.values[0][:name].empty?
+      attributes.values.each do |attributes_hash|
+        category = Category.find_or_create_by(name: attributes_hash[:name])
+        self.categories << category unless self.categories.include? category
+      end
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: 'Silly')
+Category.create(name: 'Funny')
+
+fb = User.create(username: 'FatBarry', email: 'fatbarry123@email.com')
+rs = User.create(username: 'Richard Slap', email: 'rslap123@email.com')
+
+Post.create(title: 'A Hilarious Post', content: 'Here is my post. It is very funny.')
+
+Comment.create(content: 'haha what a hilarious post', user_id: 1, post_id: 1)
